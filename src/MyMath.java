b00af0564bfccaa4ae2b8a294744415a4f7a2327
bmaus <bmaus719@mycr.redwoods.edu>
2023-03-10 17:08:58 -0800

    Completed Assignment-007 (James Bond Special)

--- src/MyMath.java
+++ src/MyMath.java
@@ -1,7 +1,41 @@
+/**
+ * @author Trevor Hartman
+ * @author Bryan Maus
+ * Date 03/10/2023
+ *
+ * @since version 1.0
+ *
+ * This program calculates the greatest common factor of 2 numbers
+ */
+
 import java.util.Scanner;
 
 public class MyMath {
+    //Fluffy stuff
+    public static final String RESET = "\u001B[0m";
+    public static final String GREEN = "\u001B[32m";
 
     public static void main(String[] args) {
+        //Initialize and collect numbers
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = s.nextInt();
+        System.out.print("\nEnter second number: ");
+        int b = s.nextInt();
+        int c =0;
+
+        //Loop to figure GCF
+        while (b!=0){
+            if(a>b){
+                c=b;
+                b=a%b;
+                a=c;
+            }
+            else {
+                b=b%a;
+            }
+        }
+        //output to screen
+        System.out.println("\nThe Greatest Common Factor is: " + GREEN + c + RESET);
     }
 }
